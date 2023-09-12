3ab56ec24a8b93c4cc42f8f574a503eaac82b085
Ethan Launay <135010522+ethlny@users.noreply.github.com>
2023-09-12 17:04:43 +0200

    ♻️: Refacto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 export class Item {
   name: string;
-  sellIn: number;
+  expiresIn: number;
   quality: number;
+  /*
+  Item with type 'QualityOverTime' will increase in quality as it ages
+  Item with type 'Conjured' will decrease in quality twice as fast as normal items
+  Item with type 'Normal' will decrease in quality as it ages
+  Item with type 'ConcertTicket', 
+    - If the concert is more than 10 days away, the quality increases by 1 each day.
+    - If the concert is 10 days or less away, the quality increases by 2 each day.
+    - If the concert is 5 days or less away, the quality increases by 3 each day.
+    - Once the concert has passed (i.e., the expiration date has reached 0), the quality drops to 0
+  */
+  type : 'QualityOverTime' | 'Conjured' | 'Normal' | 'ConcertTicket' | 'Intangible';
 
-  constructor(name, sellIn, quality) {
+  constructor(name, expiresIn, quality, type) {
     this.name = name;
-    this.sellIn = sellIn;
+    this.expiresIn = expiresIn;
     this.quality = quality;
+    this.type = type;
   }
 }
 
@@ -17,53 +29,61 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+  updateItemQuality(item: Item) {
+
+    const itemExpiresIn = item.expiresIn;
+    const itemQuality = item.quality;
+    const itemType = item.type;
+
+    if (itemType === 'QualityOverTime') {
+      if (itemQuality < 50) {
+        item.quality = itemQuality + 1;
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+    }
+
+    if (itemType === 'Conjured') {
+      if (itemQuality => 2) {
+        item.quality = itemQuality - 2;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+    }
+
+    if (itemType === 'Normal') {
+      if (itemQuality > 0) {
+        item.quality = itemQuality - 1;
+      }
+    }
+
+    if (itemType === 'ConcertTicket') {
+      if (itemQuality < 50) {
+        if (itemExpiresIn > 10) {
+          item.quality = itemQuality + 1;
+        }
+        if (itemExpiresIn <= 10 && itemExpiresIn > 5) {
+          item.quality = itemQuality + 2;
+        }
+        if (itemExpiresIn <= 5 && itemExpiresIn > 0) {
+          item.quality = itemQuality + 3;
+        }
+        if (itemExpiresIn <= 0) {
+          item.quality = 0;
         }
       }
     }
 
+    if (itemType === 'Intangible') {
+      return;
+    }
+
+    item.expiresIn = itemExpiresIn - 1;
+
+  }
+
+  updateEachItem() {
+    this.items.forEach(item => {
+      this.updateItemQuality(item);
+    });
+
     return this.items;
   }
+
 }
